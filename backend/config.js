973ea00fea62dea8feaf0a910c6834839946abfa
Mateusz <mateusz.kulinski1993@gmail.com>
2021-05-29 09:18:13 +0200

    getTeam to axios

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const hostUrl = 'api.football-data.org';
+const hostUrl = 'https://api.football-data.org';
 const token = '010ddabbb98f4571bca7d73ab1189cf5';
 const api_v = 'v2';
 
@@ -8,9 +8,15 @@ module.exports = {
     port: process.env.PORT,
     dbname: process.env.DATABASE,
     api_v: `/${api_v}`,
+    hostUrl: hostUrl,
     options: {
         host: hostUrl,
         dataType: 'json',
         headers: { 'X-Auth-Token': token },
     },
+    axiosOptions: {
+        headers: {
+            'X-Auth-Token': token,
+        }
+    }
 }
--- backend/controllers/api/teamActions.js
+++ backend/controllers/api/teamActions.js
@@ -1,27 +1,18 @@
-const { options, api_v } = require('../../config');
+const { axiosOptions, options, api_v, hostUrl } = require('../../config');
 const https = require('https');
+const axios = require('axios');
 
 class teamActions {
     //Show one particular team.
-    getTeam(req, res) {
+    async getTeam(req, res) {
         //id Team
         const id = req.params.id;
-        options.path = `/${api_v}/teams/${id}`;
-        let response = {};
-        try {
-            https.get(options, resHttps => {
-                resHttps.on('data', data => {
-                    response += data;
-                })
-                resHttps.on('end', () => {
-                    res.status(200).send(response);
-                });
-            }).on('error', error => {
-                throw new Error(error);
+        await axios.get(`${hostUrl}${api_v}/teams/${id}`, axiosOptions)
+            .then((value) => {
+                res.status(200).send(value.data);
+            }).catch((error) => {
+                res.status(error.status).json({ message: error.message });
             });
-        } catch (err) {
-            return res.status(err.status).json({ message: err.message });
-        }
     }
 
     //List one particular area.
