9ec96f113116bef3b30cdd96a641bdf2c669d8bc
Mateusz <mateusz.kulinski1993@gmail.com>
2021-05-31 19:09:33 +0200

    getLiveMatches add try catch

--- backend/controllers/api/matchActions.js
+++ backend/controllers/api/matchActions.js
@@ -14,13 +14,14 @@ module.export = {
     //List of today matches
     getLiveMatches: async(req, res) =>  {
         //id Team
+        try{
         const formatYmd = date => date.toISOString().slice(0, 10);
         let date = formatYmd(new Date());
-        let value = await axios.get(`${hostUrl}${api_v}/matches?dataFrom=${date}&dataTo=${date}`, axiosOptions)
-            .catch((error) => {
-                return res.status(error.status).json({ message: error.message });
-            });            
+        let value = await axios.get(`${hostUrl}${api_v}/matches?dataFrom=${date}&dataTo=${date}`, axiosOptions);        
         res.status(200).send(value.data);
+        }catch(error){
+            return res.status(error.status).json({ message: error.message });
+        }
     },
     //Show one particular match.
     getSingleMatch: async(req, res) => {
