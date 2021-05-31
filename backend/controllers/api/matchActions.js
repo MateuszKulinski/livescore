e0296dae53661fb5f66a5bc22bcc643dd0fc93e5
Mateusz <mateusz.kulinski1993@gmail.com>
2021-05-31 19:12:33 +0200

    matchactions add try catch

--- backend/controllers/api/matchActions.js
+++ backend/controllers/api/matchActions.js
@@ -1,24 +1,25 @@
 const { axiosOptions, api_v, hostUrl } = require('../../config');
 const axios = require('axios');
 
-module.export = {
+module.exports = {
 //List of live matches
     getLiveMatches: async(req, res) =>  {
         //id Team
-        let value = await axios.get(`${hostUrl}${api_v}/matches?status=LIVE`, axiosOptions)
-            .catch((error) => {
-                return res.status(error.status).json({ message: error.message });
-            });
-        res.status(200).send(value.data);
+        try{
+            let value = await axios.get(`${hostUrl}${api_v}/matches?status=LIVE`, axiosOptions);
+            res.status(200).send(value.data);
+        }catch(error){
+            return res.status(error.status).json({ message: error.message });
+        }
     },
     //List of today matches
     getLiveMatches: async(req, res) =>  {
         //id Team
         try{
-        const formatYmd = date => date.toISOString().slice(0, 10);
-        let date = formatYmd(new Date());
-        let value = await axios.get(`${hostUrl}${api_v}/matches?dataFrom=${date}&dataTo=${date}`, axiosOptions);        
-        res.status(200).send(value.data);
+            const formatYmd = date => date.toISOString().slice(0, 10);
+            let date = formatYmd(new Date());
+            let value = await axios.get(`${hostUrl}${api_v}/matches?dataFrom=${date}&dataTo=${date}`, axiosOptions);        
+            res.status(200).send(value.data);
         }catch(error){
             return res.status(error.status).json({ message: error.message });
         }
@@ -26,21 +27,23 @@ module.export = {
     //Show one particular match.
     getSingleMatch: async(req, res) => {
         //id match
-        const id = req.params.id;
-        let value = await axios.get(`${hostUrl}${api_v}/matches/${id}`, axiosOptions)
-            .catch((error) => {
-                return res.status(error.status).json({ message: error.message });
-            }); 
-        res.status(200).send(value.data);
+        try{
+            const id = req.params.id;
+            let value = await axios.get(`${hostUrl}${api_v}/matches/${id}`, axiosOptions); 
+            res.status(200).send(value.data);
+        }catch(error){
+            return res.status(error.status).json({ message: error.message });
+        }
     },
     //Show all matches for a particular player.
     getSinglePlayerMatches: async(req, res) => {
         //id match
-        const id = req.params.id;
-        let value = await axios.get(`${hostUrl}${api_v}/players/${id}/matches`, axiosOptions)
-            .catch((error) => {
-                return res.status(error.status).json({ message: error.message });
-            }); 
-        res.status(200).send(value.data);
+        try{
+            const id = req.params.id;
+            let value = await axios.get(`${hostUrl}${api_v}/players/${id}/matches`, axiosOptions); 
+            res.status(200).send(value.data);
+        }catch(error){
+            return res.status(error.status).json({ message: error.message });
+        }
     },
 }
